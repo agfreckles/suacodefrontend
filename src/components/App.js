5b822e78bdef1f7373d4d1e459fd230d7e5ba644
agfreckles <oluueli@gmail.com>
2022-12-20 09:25:47 +0000

    Setup Semantic UI for main App.js. Create NotesList component and assign it to App.js

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NotesList from "./notes/NotesList";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <NotesList />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/notes/NotesList.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const NotesList = () => {
+  return <div>Notes List !</div>;
+};
+export default NotesList;
